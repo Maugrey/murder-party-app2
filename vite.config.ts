d8853cea8aafb9bbb75ad53984df2fbd3a5e95fc
Maugrey <maumo01@hotmail.com>
2025-04-25 12:04:36 +0200

    fix tailwind again

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
-import postcss from 'postcss';
-import tailwindcss from '@tailwindcss/postcss7-compat';
-import React from '@vitejs/plugin-react';
-// Ajout d'une déclaration pour le module manquant
-declare module '@tailwindcss/postcss7-compat';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [React()],
-  css: {
-    postcss: {
-      plugins: [postcss(), tailwindcss()],
-    },
-  },
+  plugins: [react(),tailwindcss()]
 });
